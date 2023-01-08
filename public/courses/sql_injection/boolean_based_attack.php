f10119ccbaa60f4518c871283377f8db96a6eee1
caidenceus <caidentp@icloud.com>
2023-01-08 13:14:57 -0600

    Begin to add code for courses

--- public/courses/sql_injection/boolean_based_attack.php
+++ public/courses/sql_injection/boolean_based_attack.php
@@ -1,5 +1,6 @@
 <?php require_once 'relative_init.php'; ?>
 <?php require_once SHARED_PATH . 'head.php'; ?>
 <?php require_once SHARED_PATH . 'page_header.php'; ?>
+<?php require_once SHARED_PATH . 'legal_disclaimer.php'; ?>
 
 <?php require_once SHARED_PATH . 'page_footer.php'; ?>
--- public/courses/sql_injection/sqlmap_basics.php
+++ public/courses/sql_injection/sqlmap_basics.php
@@ -1,5 +1,12 @@
 <?php require_once 'relative_init.php'; ?>
 <?php require_once SHARED_PATH . 'head.php'; ?>
+
+<?php echo '<link rel="stylesheet" href="' . CSS_PATH . 'course.css">'; ?>
+
 <?php require_once SHARED_PATH . 'page_header.php'; ?>
+<?php require_once SHARED_PATH . 'legal_disclaimer.php'; ?>
+
+    <div class="course-top-level-container">
+    </div>
 
 <?php require_once SHARED_PATH . 'page_footer.php'; ?>
