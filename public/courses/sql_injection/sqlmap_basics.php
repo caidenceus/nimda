e610c9560e68bcc393d6ea8f64cbb02add8698db
caidenceus <caidentp@icloud.com>
2023-01-08 13:20:47 -0600

    Add basic course page css

--- public/courses/sql_injection/sqlmap_basics.php
+++ public/courses/sql_injection/sqlmap_basics.php
@@ -7,6 +7,13 @@
 <?php require_once SHARED_PATH . 'legal_disclaimer.php'; ?>
 
     <div class="course-top-level-container">
+      <h1 class="title">What is SQL map?</h1>
+      <p class="course-text">
+        Sqlmap is an ethical hacking tool written in Python that is used to help
+        automate detecting and exploiting SQL vulnerabilities. Its features 
+        range from detecting vulnerable HTML form parameters, to enumerating
+        database users, to completely taking over a database server.        
+      </p>
     </div>
 
 <?php require_once SHARED_PATH . 'page_footer.php'; ?>
