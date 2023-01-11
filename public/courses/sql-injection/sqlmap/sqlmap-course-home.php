913c5dd14fefca79e6cd6f2dc5a6422902330224
caidenceus <caidentp@icloud.com>
2023-01-11 12:50:41 -0600

    Add title to tutorials section for sqlmap

--- public/courses/sql-injection/sqlmap/sqlmap-course-home.php
+++ public/courses/sql-injection/sqlmap/sqlmap-course-home.php
@@ -12,13 +12,13 @@ require_once SHARED_PATH . 'legal_disclaimer.php';
     <div class="top-level-container course-home-root-container">
       <div class="course-home-container course-home-container-left">
         <h1 class="title">Sqlmap</h1>
-        <p class="course-text">
+        <p>
           Sqlmap is an open source ethical hacking tool written in Python that is 
           used to help automate detecting and exploiting SQL vulnerabilities.
           Its features range from detecting vulnerable HTML form parameters, to 
           enumerating database users, to completely taking over a database server.      
         </p>
-        <p class="course-text">
+        <p>
           At the time of this writing, sqlmap fully supports six different 
           injection techniques, which are Boolean-based blind, 
           time-based blind, error-based, UNION query-based, stacked queries and 
@@ -28,6 +28,7 @@ require_once SHARED_PATH . 'legal_disclaimer.php';
       </div> <!-- Left half -->
 
       <div class="course-home-container course-home-container-right">
+        <h1 class="title">Tutorials</h1>
         <?php
             $tutorials = array(
                 'Install sqlmap',
