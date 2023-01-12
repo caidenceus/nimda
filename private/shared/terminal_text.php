e6cd0d8ecf3bd1c409a6e0ee50efc7d54a4b46a7
caidenceus <caidentp@icloud.com>
2023-01-11 21:35:57 -0600

    Add kali terminal animation

--- private/shared/terminal_text.php
+++ private/shared/terminal_text.php
@@ -1,11 +1,12 @@
 <?php
-if (!isset($terminal_text)) { $terminal_text = "sudo apt -y install kali-root-login"; }
+if (!isset($terminal_text)) { $terminal_text = "sudo apt-get -y install kali-root-login"; }
 
 // We need two divs for the animation. The first is to set the background color
 // of the entier width of the screen to black. The second is an inline-block div
 // so that the cursor ends at the end of the text
 echo '<div class="top-level-container terminal-text-container">';
-echo '<div class="terminal-prompt"><p>kali@kali:~$ </p></div>';
+echo '<div class="terminal-prompt">';
+echo '<p>kali@kali<span style="color: white">:</span><span style="color: #5ebdab">~</span><span style="color: white">$</span>&nbsp;</p></div>';
 echo '<div class="terminal-text"><p>' . $terminal_text . '</p></div>';
 echo '</div>';
 ?>
