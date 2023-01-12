d0d75d4ace44ac30d9063faa41f84b6bc6549012
caidenceus <caidentp@icloud.com>
2023-01-12 10:52:57 -0600

    Modify header of tutorial pages

--- private/shared/tutorial_navigation.php
+++ private/shared/tutorial_navigation.php
@@ -5,7 +5,7 @@ if (!isset($next)) { $next = ""; }
 
 echo '<div class="top-level-container tutorial-navigation">';
 echo '<a href="' . $previous . '"><button class="tutorial-navigation-button red-background red-button-hover-invert">Previous</button></a>';
-echo '<a href="' . $home . '"><button class="tutorial-navigation-button yellow-background yellow-button-hover-invert">Home</button></a>';
+echo '<a href="' . $home . '"><button class="tutorial-navigation-button yellow-background yellow-button-hover-invert">Course Home</button></a>';
 echo '<a href="' . $next . '"><button class="tutorial-navigation-button blue-background blue-button-hover-invert">Next</button></a>';
 echo '</div>';
 ?>
--- public/courses/sql-injection/sqlmap/install-sqlmap.php
+++ public/courses/sql-injection/sqlmap/install-sqlmap.php
@@ -9,6 +9,8 @@ include_once SHARED_PATH . 'page_header.php';
 $terminal_text = 'sudo apt-get install -y python3';
 include SHARED_PATH . 'terminal_text.php';
 include_once SHARED_PATH . 'legal_disclaimer.php';
+$terminal_text = 'python3 --version';
+include SHARED_PATH . 'terminal_text.php';
 
 $home = 'sqlmap-course-home.php';
 $next = 'sqlmap-basic-commands.php';
