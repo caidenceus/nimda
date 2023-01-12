b596cf00297b89587503d5fee414807905a6c783
caidenceus <caidentp@icloud.com>
2023-01-12 08:13:11 -0600

    Change style for titles and subtitles

--- private/shared/page_header.php
+++ private/shared/page_header.php
@@ -1,6 +1,13 @@
   </head>  <!-- Head is opened in head.php -->
   <body>
     <div class="top-level-container">
-      <h1 class="title">White hat hacking</h1>
-      <p class="header-tagline">Ethical hacking | labs | techniques | tools</p>
+      <h1 class="title page-header-title">White hat hacking</h1>
+
+      <ul class="page-header-buttons">
+        <li><a class="red-button"><button>Home</button></a></li>
+        <li><a class="yellow-button"><button>About Me</button></a></li>
+        <li><a class="blue-button"><button>Udemy</button></a></li>
+      </ul>
+
+      <p class="header-tagline">Ethical hacking | labs | tools | techniques </p>
     </div>
