77f8dd404ba7c632c470d8fb2d1155d26399e5a4
caidenceus <caidentp@icloud.com>
2023-01-13 16:55:59 -0600

    Add terminal to about me page

--- public_html/about-me.php
+++ public_html/about-me.php
@@ -6,6 +6,8 @@ include_once SHARED_PATH . 'head.php';
 echo '<link rel="stylesheet" href="' . CSS_PATH . 'about_me.css">';
 
 include_once SHARED_PATH . 'page_header.php';
+$terminal_text = 'git checkout about-me';
+include SHARED_PATH . 'terminal_text.php';
 ?>
     <div class="page-heading top-level-container">
       <div class="profile-picture">
@@ -15,6 +17,31 @@ include_once SHARED_PATH . 'page_header.php';
         <h1>Caiden Pyle - Vulnerability Analysis \ Quality Assurance.</h1>
       </div>
     </div>
-<?php include SHARED_PATH . 'terminal_text.php'; ?>
+
+    <?php 
+    $terminal_text = 'sudo apt-get install -y kali-root-login';
+    include SHARED_PATH . 'terminal_text.php';
+    ?>
+
+    <div class="top-level-container">
+      <h1 class="title">Areas of Expertise</h1>
+      <div class="expertise-container">
+      <?php
+      $areas = array(
+          'Agile Software Development',
+          'Scrum & Agile Methodologies',
+          'Consuming REST APIs',
+          'Software Development Lifecycle',
+          'Web Application Design & Development',
+          'Software Testing',
+          'Socket Programming'
+      );
+      
+      foreach ($areas as $area) {
+          echo '<div class="expertise-area"><p>' . $area . '</p></div>';
+      }
+      ?>
+      </div>
+    </div>
 
 <?php include_once SHARED_PATH . 'page_footer.php'; ?>
