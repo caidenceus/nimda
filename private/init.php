dacc21e0ace5989765451e59336e6293b42c60bd
caidenceus <caidentp@icloud.com>
2023-01-13 14:53:09 -0600

    Rename public folder

--- private/init.php
+++ private/init.php
@@ -3,11 +3,11 @@
 
     define("PRIVATE_PATH", dirname(__FILE__) . "/");
     define("PROJECT_PATH", dirname(PRIVATE_PATH));
-    define("PUBLIC_PATH", PROJECT_PATH . "/public/");
+    define("PUBLIC_PATH", PROJECT_PATH . "/public_html/");
     define("SHARED_PATH", PRIVATE_PATH . "/shared/");
 
     // Page links
-    define("HTTP_PUBLIC", "http://192.168.1.75/public/"); // dev location
+    define("HTTP_PUBLIC", "http://192.168.1.75/public_html/"); // dev location
     define("TUTORIAL_LINK", HTTP_PUBLIC . "index.php#tutorials");
     define("UDEMY_LINK", "https://www.udemy.com/");
     define("BUY_ME_A_COFFEE_LINK", "https://www.buymeacoffee.com/");
