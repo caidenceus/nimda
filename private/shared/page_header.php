0bed3836e1a595b98a5c5c70be8632ba3f70b30c
caidenceus <caidentp@icloud.com>
2023-01-15 13:16:53 -0600

    Fix page header wrapping issue

--- private/shared/page_header.php
+++ private/shared/page_header.php
@@ -1,15 +1,17 @@
   </head>  <!-- Head is opened in head.php -->
   <body>
     <div class="top-level-container">
-      <h1 class="title page-header-title">White hat hacking</h1>
+      <div>
+        <h1 class="title page-header-title">White hat hacking</h1>
 
-      <ul class="page-header-buttons">
-        <?php
-        echo '<li><a><button class="blue-background blue-button-hover-invert">Udemy</button></a></li>';
-        echo '<li><a href="' . HTTP_PUBLIC . 'about-me.php"><button class="yellow-background yellow-button-hover-invert">About Me</button></a></li>';
-        echo '<li><a href="' . HTTP_PUBLIC . 'index.php"><button class="red-background red-button-hover-invert">Home</button></a></li>';
-        ?>
-      </ul>
+        <ul class="page-header-buttons">
+          <?php
+              echo '<li><a href="' . HTTP_PUBLIC . 'index.php"><button class="red-background red-button-hover-invert">Home</button></a></li>';
+              echo '<li><a href="' . HTTP_PUBLIC . 'about-me.php"><button class="yellow-background yellow-button-hover-invert">About Me</button></a></li>';
+              echo '<li><a><button class="blue-background blue-button-hover-invert">Udemy</button></a></li>';
+          ?>
+        </ul>
+      </div>
 
       <p class="header-tagline">Ethical hacking | labs | tools | techniques </p>
     </div>
