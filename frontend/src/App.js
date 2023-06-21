a6b4367aacd7af7fbe1b27a822561dcb177077d6
Caiden Pyle <cpyle@novacoast.com>
2023-06-20 23:32:56 -0500

    Add basic subject code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import LeftSidebar from './components/common/LeftSidebar.js';
 
+import Dashboard from './components/pages/dashboard/Dashboard.js';
+
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div id="app" style={{ display: 'flex', height: '100%' }}>
       <LeftSidebar />
-      <main className="content" style={{ width: "100%" }}>
+      <main className="content" style={{ width: "100%", padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
       </main>
     </div>
   );
--- frontend/src/components/pages/dashboard/Dashboard.js
+++ frontend/src/components/pages/dashboard/Dashboard.js
@@ -1,7 +1,65 @@
-const Dashboard = () => {
+const Subject = ({ title, subtitle }) => {
   return (
+    <div
+      style={{
+        gridColumn: 'span 1',
+        gridRow: 'span 1',
 
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+
+        border: '1px solid #333',
+        padding: '0 20px'
+      }}
+    >
+      <h2>{ title }</h2>
+      <p>{ subtitle }</p>
+    </div>
   );
-}
+};
+
+
+const Dashboard = () => {
+  const subjects = [
+    {
+      title: 'Calculus I',
+      subtitle: 'Basic limits and derivatives'
+    },
+    {
+      title: 'Calculus II',
+      subtitle: 'Integrals and antiderivatives'
+    },
+    {
+      title: 'Calculus III',
+      subtitle: 'Calculus derivatives and integrals in 3D'
+    },
+    {
+      title: 'Number Theory',
+      subtitle: 'Theory of the integers'
+    },
+    {
+      title: 'Linear Algebra',
+      subtitle: 'Algebra of Matricies'
+    }
+  ];
+
+  return (
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns:
+        'repeat(3, 1fr)',
+        gridAutoRows: '100px',
+        gap: '20px'
+      }}>
+      <Subject title="Calculus 1" subtitle="Basic limits and derivatives" />
+      <Subject title="Calculus 2" subtitle="Integrals and antiderivatives" />
+      <Subject title="Calculus 3" subtitle="Calculus derivatives and integrals in 3D" />
+      <Subject title="Number Theory" subtitle="Theory of the integers" />
+      <Subject title="Linear Algebra" subtitle="Algebra of Matricies" />
+    </div>
+  );
+};
 
 export default Dashboard;
